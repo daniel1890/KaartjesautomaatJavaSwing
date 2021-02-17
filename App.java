70c6d9291faf6ced8bba7c54e56fc9cdba4a045b
Daniël <59751559+daniel1890@users.noreply.github.com>
2021-02-17 14:02:25 +0100

    Update App.java

--- App.java
+++ App.java
@@ -4,9 +4,7 @@ import javax.swing.SwingUtilities;
 public class App {
 
 	public static void main(String[] args) {
-		
 		SwingUtilities.invokeLater(new Runnable() {
-
 			public void run() {
 				new MainFrame();
 			}
